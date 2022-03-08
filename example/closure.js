337aa0dabd3cb3f5c095c9c9104d18fe596c2d2a
DevFrog92 <yongsu1481@gmail.com>
2022-03-08 10:43:50 +0900

    add closure example

--- example/closure.js
+++ example/closure.js
@@ -1,38 +1,38 @@
 // bar is not closure
 
-// function foo() {
-//   const x = 1;
+function foo() {
+  const x = 1;
 
-//   function bar() {
-//     const z = 10;
+  function bar() {
+    const z = 10;
 
-//     debugger;
+    debugger;
 
-//     console.log(z)
-//   }
+    console.log(z)
+  }
 
-//   return bar
-// }
+  return bar
+}
 
-// const bar = foo();
-// bar();
+const bar = foo();
+bar();
 
 // bar is closure
 
-// function foo() {
-//   const x = 1;
+function foo() {
+  const x = 1;
 
-//   function bar() {
-//     debugger;
+  function bar() {
+    debugger;
 
-//     console.log(x)
-//   }
+    console.log(x)
+  }
 
-//   return bar
-// }
+  return bar
+}
 
-// const bar = foo();
-// bar();
+const bar2 = foo();
+bar();
 
 // information hiding
 
@@ -47,3 +47,24 @@ const increase = (function() {
 console.log(increase());
 console.log(increase());
 console.log(increase());
+
+
+const funcs = []
+
+for(let i = 0; i < 3; i++){
+  funcs[i] = (function(id){
+    return function() {
+      return id
+    }
+  }(i))
+}
+
+for(var i = 0; i < 3; i++){
+  funcs[i] = function() {
+      return i
+    }
+}
+
+for(let j = 0; j< funcs.length; j++) {
+  console.log(funcs[j]())
+}
